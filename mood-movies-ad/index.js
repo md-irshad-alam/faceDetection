bc760d5c5b04725a0763204bc3ebf39707647d2f
irshad <almairsh0205@gmail.com>
2025-05-09 13:45:53 +0530

    updated

--- mood-movies-ad/index.js
+++ mood-movies-ad/index.js
@@ -5,31 +5,37 @@ const scanButton = document.querySelector(".scan-button"); // Get the scan butto
 
 const movieMap = {
   Happy: {
-    title: "Welcome",
-    poster:
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR4aqGT0d_m20aT9GwiZtUqSVCCGWpGH9NFWmWUirmYr7gB6eIRn-ZhO1j7hbMCMvwosbbydQ",
-  },
-  Excited: {
-    title: "Inception",
-    poster:
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQovCe0H45fWwAtV31ajOdXRPTxSsMQgPIQ3lcZX_mAW0jXV3kH",
-  },
-  Neutral: {
-    title: "Forrest Gump",
-    poster:
-      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcScf5su68o8oOp0D89ESlb3_8RW2ge3ZWIPFv_OBVSObb680o3H",
-  },
-  Angry: {
-    title: "Gladiator",
-    poster:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFabKsWv9ru_kpMttjPf2493GGI7L3LpW3XjgPTE9FyHdNDIwV",
+    name: "Welcome",
+    link: "https://www.primevideo.com/detail/0MJFLZHIV04F9V9V21RAY2Z8ZZ/",
+    thumbnail:
+      "https://m.media-amazon.com/images/S/pv-target-images/af13e1c59556eb143d2b213c9f95567677f409033d4c9619c553367d71bee982._SX1920_FMwebp_.jpg",
   },
   Sad: {
-    title: "Inside Out",
-    poster:
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR7IE8YJaJlh3CW-KYU745oE2WzjyvRgKwNyAwe73di_U0uuD5Q",
+    name: "Call me Bae",
+    link: "https://www.primevideo.com/detail/0TF2BODX83KZOWTP08NXFE897E/",
+    thumbnail:
+      "https://m.media-amazon.com/images/S/pv-target-images/0cb7ac74d1d6e8eb2e3d59aa5354359714eb54d84fcfaa616d9de19d64b492ca._SX1920_FMwebp_.jpg",
+  },
+  Excited: {
+    name: "Citadel Honey Bunny",
+    link: "https://www.primevideo.com/detail/0KYRVT4JDB957NXZO72E2MIFW5",
+    thumbnail:
+      "https://m.media-amazon.com/images/S/pv-target-images/51c2c75da778c109ccc33ff293ff48f0cccc60b18c3fef8a42afe2a80e07acac._SX1920_FMwebp_.jpg",
+  },
+  Neutral: {
+    name: "Farzi",
+    link: "https://www.primevideo.com/detail/0HDHQAUF5LPWOJRCO025LFJSJI",
+    thumbnail:
+      "https://m.media-amazon.com/images/S/pv-target-images/8aed532f0875925f72c4012aab688ed409773ecbfb3b18e1a39cd9ad1a4dd485._SX1920_FMwebp_.jpg",
+  },
+  Angry: {
+    name: "Agneepath",
+    link: "https://www.primevideo.com/detail/0NU7IFXPL2WWSDHNGAR5Z1GUJE/",
+    thumbnail:
+      "https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/1863426056ae862def9a69ca76e8af54cdb6b8a5a2be1100e096e59b00060847._UX1920_.png",
   },
 };
+
 let stream = null;
 
 async function startCamera() {
